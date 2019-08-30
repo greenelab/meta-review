cb9cd37208debf88484828568fd485a937c2535a
Daniel Himmelstein <daniel.himmelstein@gmail.com>
2019-08-30 13:38:09 -0400

    Revert citations to Manubot style
    Reverts part of 040638d25b9a45ad3d060afc9689a17f4c3a3045

--- build/build.sh
+++ build/build.sh
@@ -21,7 +21,7 @@ manubot process \
   --log-level=INFO
 
 # pandoc settings
-CSL_PATH=https://github.com/citation-style-language/styles/raw/0f332db9384bfbe5f9028d82b2fb4b9570c1325b/dependent/plos-computational-biology.csl
+CSL_PATH=build/assets/style.csl
 BIBLIOGRAPHY_PATH=output/references.json
 INPUT_PATH=output/manuscript.md
 
